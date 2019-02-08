6a2c816f01d5429fcd111ea0238aa5910337e53f
ayamnova <karstenladdy@gmail.com>
2019-02-08 11:49:23 -0600

    Added Oh My Zsh installation

--- scripts/zsh-setup.sh
+++ scripts/zsh-setup.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+# install oh my zsh 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # download the script to install zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
